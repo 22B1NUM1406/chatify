a01e8caf62a6c736f945f9cb7ee019fa1a079c31
Manal123 <manal05116@gmail.com>
2026-04-03 15:07:08 +0800

    backend serve

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,24 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import authRouter from './routes/auth.route.js'
+import path from 'path';
 
 dotenv.config();
-const PORT=process.env.PORT||3000;
 
 const app=express();
 
+const PORT=process.env.PORT||3000;
+const __dirname=path.resolve();
+
 app.use("/api/auth",authRouter);
+
+//backend ni frontend dist folderiig serve hiij baina
+if(process.env.NODE_ENV==="production"){
+    app.use(express.static(path.join(__dirname,"../frontend/dist")));
+    app.get("*",(req,res)=>{
+        res.sendFile(path.join(__dirname,"../frontend/dist/index.html"));
+    });
+}
 
 app.listen(PORT,()=>{
     console.log("Server started on PORT:" +PORT);
